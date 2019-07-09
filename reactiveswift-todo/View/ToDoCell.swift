6f7c01c8871708643d231909422e6254f70ccb14
ichikawa1116 <ku.komaruha@icloud.com>
2019-07-09 20:58:09 +0900

    Implement table View

---
+++ reactiveswift-todo/View/ToDoCell.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoCell.swift
+//  reactiveswift-todo
+//
+//  Created by Manami Ichikawa on 2019/07/09.
+//  Copyright © 2019 Manami Ichikawa. All rights reserved.
+//
+
+import UIKit
+
+final class ToDoCell: UITableViewCell {
+    @IBOutlet weak private var titleLabel: UILabel!
+}
--- reactiveswift-todo/ViewController.swift
+++ reactiveswift-todo/ViewController.swift
@@ -9,13 +9,35 @@
 import UIKit
 import ReactiveSwift
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+            tableView.register(
+                UINib(
+                    nibName: "ToDoCell",
+                    bundle: nil),
+                forCellReuseIdentifier: "ToDoCell")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return 10 // TODO: fix
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "ToDoCell",
+            for: indexPath) as! ToDoCell
+        return cell
+    }
 }
 
